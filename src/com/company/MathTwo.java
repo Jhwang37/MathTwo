8bf88cf5f5c45d988d0f32fa9ffa68de34b34cb6
jhwang37 <jhwang37@gmail.com>
2020-01-22 10:59:23 -0500

    math two assignment

---
+++ src/com/company/MathTwo.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class MathTwo {
+
+    public static void main(String[] args) {
+	int myNumber = 2;
+	Float myOtherNumber = 1.7938F;
+
+        System.out.println("My first number " + myNumber);
+        System.out.println("My second number " + myOtherNumber);
+    }
+}
